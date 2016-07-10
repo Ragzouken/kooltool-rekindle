ae76e83deb43d27cdd659797e0ee21ab87eaa277
Ragzouken <ragzouken@gmail.com>
2016-07-10 09:09:35 +0100

    more animation

---
+++ Assets/Plugins/Prototyping/SetAnimatorInt.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SetAnimatorInt : MonoBehaviour 
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private string parameter;
+    [SerializeField] private int value;
+
+    public void Invoke()
+    {
+        animator.SetInteger(parameter, value);
+    }
+}
