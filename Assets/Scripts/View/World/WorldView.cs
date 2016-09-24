7c2277ed366cbb5f4e17bdcc4a9ec4922eb654aa
mark <ragzouken@gmail.com>
2016-09-24 15:51:24 +0100

    cleanup

--- Assets/Scripts/View/World/WorldView.cs
+++ Assets/Scripts/View/World/WorldView.cs
@@ -7,25 +7,15 @@ using System.Collections.Generic;
 
 public class WorldView : InstanceView<World> 
 {
-    [SerializeField] private Transform belowTileParent;
-    [SerializeField] private Transform aboveTileParent;
-
     [SerializeField] private ImageGridView backgroundView;
-
+    
     [SerializeField] private InstancePoolSetup actorSetup;
 
     public InstancePool<Actor> actors;
 
-    [SerializeField] private InstancePoolSetup testSetup;
-    public AnonymousPool<Color, SpriteRenderer> test;
-
     private void Awake()
     {
         actors = actorSetup.Finalise<Actor>(sort: false);
-
-        test = testSetup.Finalise<Color, SpriteRenderer>((c, r) => r.color = c);
-
-        test.SetActive(Enumerable.Range(0, 5).Select(i => Color.red * Random.value).ToList());
     }
 
     protected override void Configure()
